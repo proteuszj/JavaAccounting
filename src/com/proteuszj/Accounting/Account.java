be1602342f07ec02778668fa09061c023f0da558
ZJ <proteuszj@163.com>
2018-04-06 21:58:28 +0800

    update

--- src/com/proteuszj/Accounting/Account.java
+++ src/com/proteuszj/Accounting/Account.java
@@ -1,13 +1,48 @@
 
 package com.proteuszj.Accounting;
 
+import java.util.Date;
 
 /**
  * @author zhangjun
- *
+ * Base of all account class
  */
 public abstract class Account
 {
+	/**
+	 * Whether checked on UI
+	 */
+	boolean Checked;
+	
+	/**
+	 * Date when the account was created
+	 */
+	Date CreatedTime;
 
+	/**
+	 * Description of account
+	 */
+	String Description;
+	
+	/**
+	 * Issuer of the account
+	 */
+	String Issuer;
+	
+	/**
+	 * Unique Name of the account
+	 */
+	String Name;
+	
+	/**
+	 * Number of the account
+	 */
+	String Number;
+	
+	/**
+	 * Owner name of the account
+	 */
+	String Owner;
+	
 }
 
